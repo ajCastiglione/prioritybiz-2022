49c3471e15b17aae9fb3abd53e89a55be90efe98
Antonio Castiglione <antoniocastiglione10@gmail.com>
2022-08-29 14:36:45 -0400

    reconfigured how block styles are done

--- blocks/services/services.php
+++ blocks/services/services.php
@@ -9,40 +9,36 @@ $id = 'services-block';
 
 // Create class attribute allowing for custom "className" and "align" values.
 $className = 'services';
-if (!empty($block['className'])) {
-    $className .= ' ' . $block['className'];
+if ( ! empty( $block['className'] ) ) {
+	$className .= ' ' . $block['className'];
 }
-if (!empty($block['align'])) {
-    $className .= ' align' . $block['align'];
+if ( ! empty( $block['align'] ) ) {
+	$className .= ' align' . $block['align'];
 }
 
 // Load values and assign defaults.
-$services = get_field('services');
+$services = get_field( 'services' );
 
 ?>
-<section id="<?php echo esc_attr($id); ?>">
-    <div class="<?php echo esc_attr($className); ?>">
+<section id="<?php echo esc_attr( $id ); ?>">
+	<div class="<?php echo esc_attr( $className ); ?>">
 
-        <div class="services__cards">
-            <div class="row">
+		<div class="services__cards">
+			<?php
+			foreach ( $services as $service ) :
+				$img   = $service['image'];
+				$title = $service['title'];
+				$desc  = $service['description'];
+				?>
+				<div class="card-container gy-3">
+					<div class="card">
+						<img src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>" class="card-img-top">
+						<h3 class="card-title"><?php echo $title; ?></h3>
+						<div class="card-text"><?php echo $desc; ?></div>
+					</div>
+				</div>
 
-                <?php foreach ($services as $service) :
-                    $img = $service['image'];
-                    $title = $service['title'];
-                    $desc = $service['description'];
-                ?>
-                    <div class="col-sm-6 col-lg-4 col-xxl-3 gy-3">
-                        <div class="card">
-                            <img src="<?= $img['url'] ?>" alt="<?= $img['alt'] ?>" class="card-img-top">
-                            <h3 class="card-title"><?= $title ?></h3>
-                            <div class="card-text"><?= $desc ?></div>
-                        </div>
-                    </div>
-
-                <?php endforeach; ?>
-            </div>
-
-        </div>
-
-    </div>
+			<?php endforeach; ?>
+		</div>
+	</div>
 </section>
--- library/js/scripts.js
+++ library/js/scripts.js
@@ -2,10 +2,10 @@
  * Load imports here.
  */
 
-import Header from "./components/Header";
-import Forms from "./components/Forms";
+import Header from './components/Header';
+import Forms from './components/Forms';
 
-jQuery(document).ready(function ($) {
-  Header($);
-  Forms($);
-}); /* end of as page load scripts */
+jQuery( document ).ready( function( $ ) {
+	Header( $ );
+	Forms( $ );
+} ); /* end of as page load scripts */
