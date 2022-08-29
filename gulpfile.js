4ae834091ce40eeee19a1b5830c9f35d74404412
Antonio Castiglione <antoniocastiglione10@gmail.com>
2022-08-29 14:05:50 -0400

    updated to use most recent version of gulp compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require("gulp");
-const { src, dest } = require("gulp");
 const plumber = require("gulp-plumber");
 const sass = require("gulp-sass");
 const sassGlob = require("gulp-sass-glob");
@@ -10,118 +9,48 @@ const uglify = require("gulp-uglify");
 const webpack = require("webpack-stream");
 const bs = require("browser-sync");
 
-const fs = require("fs");
 const path = require("path");
-const mergeStream = require("merge-stream");
-const concat = require("gulp-concat");
-const cssnano = require("gulp-cssnano");
 const sourcemaps = require("gulp-sourcemaps");
-const gulpif = require("gulp-if");
 
-const env = process.env.NODE_ENV || "development";
-const isDevelopment = env === "development";
-
-const componentSrc = path.join(__dirname, "blocks");
-const componentDist = path.join(__dirname, "library/dist");
-
-const blocks_scss = ["blocks/**/*.scss"];
-const blocks_js = ["blocks/**/*.js"];
-const editor = ["library/scss/editor-style.scss"];
-const scss = ["library/scss/*/*.scss", "library/blocks/**/*.scss"];
-const js = ["library/js/scripts.js"];
-const jsWatch = ["library/js/**/*.js", "!library/js/dist/*.js"];
-const imgs = ["library/images/*"];
-const all = [
-  "library/*.php",
-  "*.php",
-  "*/*.php",
-  "**/**/*.php",
-  "library/js/**/*.js",
-];
-
-const checkFileExists = (file) => {
-  const formattedFile = `${componentSrc}\\${file}\\${file}.js`;
-  if (fs.existsSync(formattedFile)) {
-    return true;
-  } else {
-    return false;
-  }
+const config = {
+  bs: {
+    proxy: "http://pbz.test/",
+  },
+  sassEntry: {
+    src: path.join(__dirname, "library/scss/style.scss"),
+    dist: path.join(__dirname, "library/css"),
+  },
+  js: {
+    src: path.join(__dirname, "library/js/scripts.js"),
+    dest: path.join(__dirname, "library/dist"),
+  },
+  filesToWatch: {
+    js: ["library/js/**/*.js", "!/library/dist/*.js"],
+    sass: [
+      "library/scss/*.scss",
+      "library/scss/**/*.scss",
+      "library/scss/**/**/*.scss",
+      "library/scss/**/**/**/*.scss",
+    ],
+    global: [
+      "library/*.php",
+      "*.php",
+      "*/*.php",
+      "**/**/*.php",
+      "library/js/**/*.js",
+    ],
+  },
 };
-
-const getFolders = (dir) =>
-  fs
-    .readdirSync(dir)
-    .filter((file) => fs.statSync(path.join(dir, file)).isDirectory());
-
-gulp.task("compile-blocks-styles", function () {
-  return mergeStream(
-    ...getFolders(componentSrc).map((folder) =>
-      src(path.join(componentSrc, folder, "*.scss"))
-        .pipe(gulpif(isDevelopment, sourcemaps.init()))
-        .pipe(sass())
-        .pipe(concat(folder + ".css"))
-        .pipe(
-          postcss([
-            autoprefixer({
-              browsers: ["last 2 versions"],
-              cascade: false,
-              grid: true,
-            }),
-          ])
-        )
-        .pipe(gulpif(!isDevelopment, cssnano()))
-        .pipe(gulpif(isDevelopment, sourcemaps.write(".")))
-        .pipe(dest(path.join(componentDist, folder)))
-        .pipe(bs.stream())
-    )
-  );
-});
-
-gulp.task("compile-blocks-js", function () {
-  return mergeStream(
-    ...getFolders(componentSrc).map((folder) =>
-      src(path.join(componentSrc, folder, "*.js"))
-        .pipe(
-          gulpif(
-            checkFileExists(folder),
-            webpack({
-              mode: "production",
-              output: {
-                filename: folder + ".js",
-              },
-              module: {
-                rules: [
-                  {
-                    test: /\.(js|jsx)$/,
-                    use: ["babel-loader"],
-                    exclude: /node_modules/,
-                  },
-                ],
-              },
-            })
-          )
-        )
-        // .pipe()
-        .pipe(gulpif(isDevelopment, sourcemaps.init()))
-        .pipe(concat(folder + ".js"))
-        .pipe(gulpif(!isDevelopment, uglify()))
-        .pipe(rename({ extname: ".min.js" }))
-        .pipe(gulpif(isDevelopment, sourcemaps.write(".")))
-        .pipe(dest(path.join(componentDist, folder)))
-        .pipe(bs.stream())
-    )
-  );
-});
 
 // Compile JS
 gulp.task("scripts", () => {
   return gulp
-    .src(js)
+    .src(config.js.src)
     .pipe(
       webpack({
         mode: "production",
         output: {
-          filename: "scripts.js",
+          filename: "master.js",
         },
         module: {
           rules: [
@@ -134,18 +63,20 @@ gulp.task("scripts", () => {
         },
       })
     )
-    .pipe(gulpif(isDevelopment, sourcemaps.init()))
-    .pipe(gulpif(!isDevelopment, uglify()))
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
     .pipe(rename({ extname: ".min.js" }))
-    .pipe(gulpif(isDevelopment, sourcemaps.write(".")))
-    .pipe(gulp.dest("./library/js/dist"));
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(config.js.dest))
+    .pipe(bs.stream());
 });
 
 //Compile scss
-gulp.task("compile", () => {
+gulp.task("sass", () => {
   return gulp
-    .src("./library/scss/style.scss")
+    .src(config.sassEntry.src)
     .pipe(sassGlob())
+    .pipe(sourcemaps.init())
     .pipe(plumber())
     .pipe(
       sass({
@@ -161,75 +92,23 @@ gulp.task("compile", () => {
         }),
       ])
     )
-    .pipe(gulp.dest("./library/css"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(config.sassEntry.dist))
     .pipe(bs.stream());
-});
-
-gulp.task("compile-login", () => {
-  return gulp
-    .src("./library/scss/modules/login.scss")
-    .pipe(plumber())
-    .pipe(
-      sass({
-        outputStyle: "compressed",
-      }).on("error", sass.logError)
-    )
-    .pipe(
-      postcss([
-        autoprefixer({
-          browsers: ["last 2 versions"],
-          cascade: false,
-        }),
-      ])
-    )
-    .pipe(gulp.dest("./library/css"));
-});
-
-gulp.task("compile-editor", () => {
-  return gulp
-    .src("./library/scss/editor-style.scss")
-    .pipe(plumber())
-    .pipe(
-      sass({
-        outputStyle: "compressed",
-      }).on("error", sass.logError)
-    )
-    .pipe(
-      postcss([
-        autoprefixer({
-          browsers: ["last 2 versions"],
-          cascade: false,
-        }),
-      ])
-    )
-    .pipe(gulp.dest("./library/css"));
 });
 
 // Watch all files for compiling
 gulp.task("init", () => {
   bs.init({
-    proxy: "pbl.test",
+    proxy: config.bs.proxy,
     injectChanges: true,
-    files: all,
+    files: config.filesToWatch.global,
   });
-  gulp.watch(scss, gulp.series("compile", "compile-login"));
-  gulp.watch(jsWatch, gulp.series("scripts"));
-  gulp.watch(blocks_scss, gulp.series("compile-blocks-styles"));
-  gulp.watch(blocks_js, gulp.series("compile-blocks-js"));
-  gulp.watch(editor, gulp.series("compile-editor"));
+  gulp.watch(config.filesToWatch.sass, gulp.series("sass"));
+  gulp.watch(config.filesToWatch.js, gulp.series("scripts"));
 });
 
-gulp.task(
-  "build",
-  gulp.series(
-    "compile-blocks-styles",
-    "compile-blocks-js",
-    "compile",
-    "scripts",
-    "compile-login",
-    "compile-editor"
-  )
-);
+gulp.task("build", gulp.series("sass", "scripts"));
 
 // Start the process
-gulp.task("default", gulp.series("init"));
+gulp.task("default", gulp.series("build", "init"));
