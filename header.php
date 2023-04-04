e7cea50f315e169990bd11d5fb75fbd28cc050e9
Antonio Castiglione <antonio@minervawebdevelopment.com>
2023-04-04 16:34:24 -0400

    removed tracking meant for another site

--- header.php
+++ header.php
@@ -35,15 +35,6 @@
 
 	<?php wp_head(); ?>
 
-	<!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-795363933"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-
-		gtag('config', 'AW-795363933');
-	</script>
 
 	<?php
 	// ACF Variables
