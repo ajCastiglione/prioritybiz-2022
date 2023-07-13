4fd3017b7dc3e66ef9eaa4b7aae508545e623f03
Antonio Castiglione <antonio@minervawebdevelopment.com>
2023-07-13 19:21:59 -0400

    patch for default hero image

--- template-parts/hero/general.php
+++ template-parts/hero/general.php
@@ -6,8 +6,9 @@
  */
 
 $options    = get_field( 'hero' );
-$bg_html    = wp_get_attachment_image( $options['image']['ID'], 'full', false, array( 'class' => 'hero__bg' ) );
-$image_meta = wp_get_attachment_metadata( $options['image']['ID'] );
+$image_id   = ! empty( $options['image']['ID'] ) ? $options['image']['ID'] : get_field( 'header', 'options' )['default_hero_image']['ID'];
+$bg_html    = wp_get_attachment_image( $image_id, 'full', false, array( 'class' => 'hero__bg' ) );
+$image_meta = wp_get_attachment_metadata( $image_id );
 // Generate the upload directory uri base path for the images.
 $base_path = wp_upload_dir()['baseurl'] . '/';
 
@@ -21,7 +22,7 @@ $hero_title = ! empty( $options['title'] ) ? $options['title'] : get_the_title()
 			// Generate <source> elements for different image sizes.
 			foreach ( $image_meta['sizes'] as $size ) {
 				// Get the upload date of the image.
-				$upload_date = get_the_time( 'Y/m', $options['image']['ID'] ) . '/';
+				$upload_date = get_the_time( 'Y/m', $image_id ) . '/';
 				echo '<source media="(max-width: ' . esc_attr( $size['width'] ) . 'px)" srcset="' . esc_attr( $base_path . $upload_date . $size['file'] ) . '">';
 			}
 			echo wp_kses_post( $bg_html );
