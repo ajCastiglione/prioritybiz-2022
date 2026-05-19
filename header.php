183f57b6ad85cb9d9153251e19181ae07dc3ae43
Antonio Castiglione <antonio@minervawebdevelopment.com>
2026-05-19 11:47:26 -0400

    added tracking

--- header.php
+++ header.php
@@ -59,6 +59,10 @@
 </head>
 
 <body <?php body_class(); ?>>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NB9PNRS"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 
 	<div id="container">
 
